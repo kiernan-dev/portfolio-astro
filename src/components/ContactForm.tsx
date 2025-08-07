a9b15041cd3eac8fa8d7fa87d5c63ed5f50d7f49
kiernan-dev <kiernan.devsignr@gmail.com>
2025-08-07 06:45:12 -0500

    fix readability light/dark mod issues on the contact form page

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -56,7 +56,7 @@ const ContactForm = () => {
         <form onSubmit={HandleFormSubmit} className="Fade_Up bg-LinkBtnGradient rounded-md w-full lg:max-w-[650px] px-4 py-2 outline outline-1 outline-white/20 flex_center flex-col">
             <label
                 htmlFor="name"
-                className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2"
+                className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2 text-white"
             >
                 Name
                 <input
@@ -71,7 +71,7 @@ const ContactForm = () => {
             </label>
             <label
                 htmlFor="email"
-                className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2"
+                className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2 text-white"
             >
                 Email
                 <input
@@ -86,7 +86,7 @@ const ContactForm = () => {
             </label>
             <label
                 htmlFor="message"
-                className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2"
+                className="noCustomCursor w-full h-fit flex justify-center items-start flex-col px-1 py-2 text-white"
             >
                 Message
                 <textarea
