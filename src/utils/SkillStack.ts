7afb61063d56137a6b58fc99ce49bd205db5da6b
kiernan-dev <kiernan.devsignr@gmail.com>
2025-09-19 12:14:54 -0500

    updated tech stack content

--- src/utils/SkillStack.ts
+++ src/utils/SkillStack.ts
@@ -11,20 +11,15 @@ import {
     ReduxIcon,
     ZustandIcon,
     D3JsIcon,
-    NanostoresIcon,
     NodeIcon,
     ExpressIcon,
     MongoIcon,
     FirebaseIcon,
-    EdgeStoreIcon,
     MysqlIcon,
     PhpIcon,
     RestApiIcon,
-    GdriveIcon,
     NextAuthIcon,
     PythonIcon,
-    CppIcon,
-    JavaIcon,
     ViteIcon,
     SocketIoIcon,
     NpmIcon,
@@ -34,10 +29,8 @@ import {
     VsCodeIcon,
     FigmaIcon,
     PreactIcon,
-    GSAPIcon,
     FramerMotionIcon,
-    BunIcon,
-    RIcon
+    BunIcon
 } from "../assets/Icons";
 import { GithubIcon } from "../assets/SVGs";
 
@@ -71,7 +64,6 @@ export const FrontendStack = [
         icon: ReactQueryIcon,
         link: "https://tanstack.com/query/latest/",
     },
-    { name: "GSAP", icon: GSAPIcon, link: "https://gsap.com/" },
     { name: "Motion", icon: FramerMotionIcon, link: "https://www.framer.com/motion/" },
     { name: "Redux", icon: ReduxIcon, link: "https://redux.js.org/" },
     {
@@ -81,11 +73,6 @@ export const FrontendStack = [
     },
     { name: "D3.js", icon: D3JsIcon, link: "https://d3js.org/" },
     { name: "Preact.js", icon: PreactIcon, link: "https://preactjs.com/" },
-    {
-        name: "Nanostores",
-        icon: NanostoresIcon,
-        link: "https://github.com/nanostores/nanostores",
-    },
 ];
 export const BackendStack = [
     { name: "Node.js", icon: NodeIcon, link: "https://nodejs.org/" },
@@ -96,7 +83,6 @@ export const BackendStack = [
         icon: FirebaseIcon,
         link: "https://firebase.google.com/",
     },
-    { name: "Edge Store", icon: EdgeStoreIcon, link: "https://edgestore.dev/" },
     { name: "MySQL", icon: MysqlIcon, link: "https://www.mysql.com/" },
     { name: "PHP", icon: PhpIcon, link: "https://www.php.net/" },
     {
@@ -105,33 +91,13 @@ export const BackendStack = [
         link: "https://developer.mozilla.org/en-US/docs/Glossary/REST",
     },
     {
-        name: "Drive API",
-        icon: GdriveIcon,
-        link: "https://developers.google.com/drive",
-    },
-    {
         name: "Auth.js",
         icon: NextAuthIcon,
         link: "https://authjs.dev/",
     },
+    { name: "Python", icon: PythonIcon, link: "https://www.python.org/" },
 ];
 
-export const ProgrammingStack = [
-    {
-        name: "JavaScript",
-        icon: JsIcon,
-        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    },
-    {
-        name: "TypeScript",
-        icon: TsIcon,
-        link: "https://www.typescriptlang.org/",
-    },
-    { name: "Python", icon: PythonIcon, link: "https://www.python.org/" },
-    { name: "C++", icon: CppIcon, link: "https://isocpp.org/" },
-    { name: "Java", icon: JavaIcon, link: "https://www.oracle.com/java/" },
-    { name: "R", icon: RIcon, link: "https://www.r-project.org/" },
-];
 
 export const ToolsStack = [
     { name: "NPM", icon: NpmIcon, link: "https://www.npmjs.com/" },
