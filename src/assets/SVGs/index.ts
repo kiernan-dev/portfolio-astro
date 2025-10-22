b044c74442aa185e514fae296d0edb3a0b7c2f87
kiernan-dev <kiernan.devsignr@gmail.com>
2025-10-22 15:07:00 -0500

    remove unused imports

--- src/assets/SVGs/index.ts
+++ src/assets/SVGs/index.ts
@@ -1,12 +1,3 @@
 export { default as PurpleWave } from "./purpleWave.svg"
 export { default as LogoIcon } from "../webp/k-favicon.webp"
-
-//Social Icons
-export { default as GithubIcon } from "./github.svg"
-export { default as GoogleIcon } from "./google.svg"
-export { default as GmailIcon } from "./gmail.svg"
-export { default as InstagramIcon } from "./instagram.svg"
-export { default as LinkedinIcon } from "./linkedin.svg"
-export { default as TwitterIcon } from "./twitter.svg"
-export { default as FaceBookIcon } from "./facebook.svg"
-export { default as DiscordIcon } from "./discord.svg"
+export { default as GithubIcon } from "./Github.svg"
